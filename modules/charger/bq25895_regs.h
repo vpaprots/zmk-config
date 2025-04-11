c25af56381c6acb88ded2b8455c363c5bed4cf04
Volodymyr Paprotski <git@papro.ca>
2025-04-11 14:14:41 -0400

    checkpoint: BQ25895 driver sorta works, STRDY keymap

--- modules/charger/bq25895_regs.h
+++ modules/charger/bq25895_regs.h
@@ -1,109 +1,120 @@
 // BOOL(name, register, bit)
-// INT (name, register, startbit, endbit, offset, multiplier, unit)
+// INT (name, register, startbit, endbit, offset, multiplier)
 
-// reg00 
-BOOL(high_impedance_mode, EN_HIZ, 0, 7);
-BOOL(ilim_pin, EN_ILIM, 0, 6);
-INT (current_limit, IINLIM, 0, 0, 5, linear_val(100, 50, mA));
-// 100mA + 50A*current_limit
+#undef BQ_CFG
+#define BQ_CFG(extra) \
+INT(batlowv, 6, 1, 1, 2800, 200, extra) \
+INT(iprechg, 5, 4, 7, 64, 64, extra) \
+INT(ichg, 4, 0, 6, 0, 64, extra) \
+INT(iterm, 5, 0, 3, 64, 64, extra) \
+INT(sys_min, 3, 1, 3, 3000, 100, extra) \
+BOOL(otg_config, 3, 5, extra) \
+BOOL(ico_en, 2, 4, extra) \
+BOOL(hvdcp_en, 2, 3, extra) \
+BOOL(maxc_en, 2, 2, extra)
 
-// reg01
-INT (boost_hot, 1, 6, 7, boost_hot_val);
-BOOL(boost_cold, 1, 5);
-INT (input_voltage_limit_offset, 1, 0, 4, 0, 100);
+#undef BQ_ADC
+#define BQ_ADC(extra) \
+INTA(batv, 0xE, 0, 6, 2304, 20, extra) \
+INTA(sysv, 0xF, 0, 6, 2304, 20, extra) \
+INTA(vbusv, 0x11, 0, 6, 2600, 100, extra) \
+INTA(ichgr, 0x12, 0, 6, 0, 50, extra)
+// INT(tspct:   ADC conversion of TS Voltage (TS) as percentage of REGN
 
-// 	/* REG00 */
-[F_EN_HIZ]		= REG_FIELD(0x00, 7, 7),
-[F_EN_ILIM]		= REG_FIELD(0x00, 6, 6),
-[F_IINLIM]		= REG_FIELD(0x00, 0, 5),
-/* REG01 */
-[F_BHOT]		= REG_FIELD(0x01, 6, 7),
-[F_BCOLD]		= REG_FIELD(0x01, 5, 5),
-[F_VINDPM_OFS]		= REG_FIELD(0x01, 0, 4),
-/* REG02 */
-[F_CONV_START]		= REG_FIELD(0x02, 7, 7),
-[F_CONV_RATE]		= REG_FIELD(0x02, 6, 6),
-[F_BOOSTF]		= REG_FIELD(0x02, 5, 5),
-[F_ICO_EN]		= REG_FIELD(0x02, 4, 4),
-[F_HVDCP_EN]		= REG_FIELD(0x02, 3, 3),  // reserved on BQ25896
-[F_MAXC_EN]		= REG_FIELD(0x02, 2, 2),  // reserved on BQ25896
-[F_FORCE_DPM]		= REG_FIELD(0x02, 1, 1),
-[F_AUTO_DPDM_EN]	= REG_FIELD(0x02, 0, 0),
-/* REG03 */
-[F_BAT_LOAD_EN]		= REG_FIELD(0x03, 7, 7),
-[F_WD_RST]		= REG_FIELD(0x03, 6, 6),
-[F_OTG_CFG]		= REG_FIELD(0x03, 5, 5),
-[F_CHG_CFG]		= REG_FIELD(0x03, 4, 4),
-[F_SYSVMIN]		= REG_FIELD(0x03, 1, 3),
-[F_MIN_VBAT_SEL]	= REG_FIELD(0x03, 0, 0), // BQ25896 only
-/* REG04 */
-[F_PUMPX_EN]		= REG_FIELD(0x04, 7, 7),
-[F_ICHG]		= REG_FIELD(0x04, 0, 6),
-/* REG05 */
-[F_IPRECHG]		= REG_FIELD(0x05, 4, 7),
-[F_ITERM]		= REG_FIELD(0x05, 0, 3),
-/* REG06 */
-[F_VREG]		= REG_FIELD(0x06, 2, 7),
-[F_BATLOWV]		= REG_FIELD(0x06, 1, 1),
-[F_VRECHG]		= REG_FIELD(0x06, 0, 0),
-/* REG07 */
-[F_TERM_EN]		= REG_FIELD(0x07, 7, 7),
-[F_STAT_DIS]		= REG_FIELD(0x07, 6, 6),
-[F_WD]			= REG_FIELD(0x07, 4, 5),
-[F_TMR_EN]		= REG_FIELD(0x07, 3, 3),
-[F_CHG_TMR]		= REG_FIELD(0x07, 1, 2),
-[F_JEITA_ISET]		= REG_FIELD(0x07, 0, 0), // reserved on BQ25895
-/* REG08 */
-[F_BATCMP]		= REG_FIELD(0x08, 5, 7),
-[F_VCLAMP]		= REG_FIELD(0x08, 2, 4),
-[F_TREG]		= REG_FIELD(0x08, 0, 1),
-/* REG09 */
-[F_FORCE_ICO]		= REG_FIELD(0x09, 7, 7),
-[F_TMR2X_EN]		= REG_FIELD(0x09, 6, 6),
-[F_BATFET_DIS]		= REG_FIELD(0x09, 5, 5),
-[F_JEITA_VSET]		= REG_FIELD(0x09, 4, 4), // reserved on BQ25895
-[F_BATFET_DLY]		= REG_FIELD(0x09, 3, 3),
-[F_BATFET_RST_EN]	= REG_FIELD(0x09, 2, 2),
-[F_PUMPX_UP]		= REG_FIELD(0x09, 1, 1),
-[F_PUMPX_DN]		= REG_FIELD(0x09, 0, 0),
-/* REG0A */
-[F_BOOSTV]		= REG_FIELD(0x0A, 4, 7),
-[F_BOOSTI]		= REG_FIELD(0x0A, 0, 2), // reserved on BQ25895
-[F_PFM_OTG_DIS]		= REG_FIELD(0x0A, 3, 3), // BQ25896 only
-/* REG0B */
-[F_VBUS_STAT]		= REG_FIELD(0x0B, 5, 7),
-[F_CHG_STAT]		= REG_FIELD(0x0B, 3, 4),
-[F_PG_STAT]		= REG_FIELD(0x0B, 2, 2),
-[F_SDP_STAT]		= REG_FIELD(0x0B, 1, 1), // reserved on BQ25896
-[F_VSYS_STAT]		= REG_FIELD(0x0B, 0, 0),
-/* REG0C */
-[F_WD_FAULT]		= REG_FIELD(0x0C, 7, 7),
-[F_BOOST_FAULT]		= REG_FIELD(0x0C, 6, 6),
-[F_CHG_FAULT]		= REG_FIELD(0x0C, 4, 5),
-[F_BAT_FAULT]		= REG_FIELD(0x0C, 3, 3),
-[F_NTC_FAULT]		= REG_FIELD(0x0C, 0, 2),
-/* REG0D */
-[F_FORCE_VINDPM]	= REG_FIELD(0x0D, 7, 7),
-[F_VINDPM]		= REG_FIELD(0x0D, 0, 6),
-/* REG0E */
-[F_THERM_STAT]		= REG_FIELD(0x0E, 7, 7),
-[F_BATV]		= REG_FIELD(0x0E, 0, 6),
-/* REG0F */
-[F_SYSV]		= REG_FIELD(0x0F, 0, 6),
-/* REG10 */
-[F_TSPCT]		= REG_FIELD(0x10, 0, 6),
-/* REG11 */
-[F_VBUS_GD]		= REG_FIELD(0x11, 7, 7),
-[F_VBUSV]		= REG_FIELD(0x11, 0, 6),
-/* REG12 */
-[F_ICHGR]		= REG_FIELD(0x12, 0, 6),
-/* REG13 */
-[F_VDPM_STAT]		= REG_FIELD(0x13, 7, 7),
-[F_IDPM_STAT]		= REG_FIELD(0x13, 6, 6),
-[F_IDPM_LIM]		= REG_FIELD(0x13, 0, 5),
-/* REG14 */
-[F_REG_RST]		= REG_FIELD(0x14, 7, 7),
-[F_ICO_OPTIMIZED]	= REG_FIELD(0x14, 6, 6),
-[F_PN]			= REG_FIELD(0x14, 3, 5),
-[F_TS_PROFILE]		= REG_FIELD(0x14, 2, 2),
-[F_DEV_REV]		= REG_FIELD(0x14, 0, 1)
+#undef BQ_ACTION
+#define BQ_ACTION(extra) \
+BOOLF(conv_start, 2, 7, extra) \
+BOOLF(wd_rst, 3, 6, extra) \
+BOOLF(batfet_dis, 9, 5, extra) \
+BOOLF(reg_rst, 0x14, 7, extra)
+
+// // Reg0 (R/W):
+// - EN_HIZ:  Enable HIZ Mode (disabled)
+// - EN_ILIM: Enable ILIM Pin (enabled)
+// - IINLIM:  Input Current Limit 0.1A-3.5A (0.5A, changed automaticallly after input source type detection is completed)
+// Reg1 (R/W)
+// - BHOT:      Boost Mode Hot Temperature Monitor Threshold (34.75%)
+// - BCOLD:     Boost Mode Cold Temperature Monitor Threshold (77%)
+// - VINDPM_OS: Input Voltage Limit Offset 0V - 3.1V (0.5V, used to calculate VINDPM threshold)
+// Reg2 (R/W)
+// - CONV_START:   ADC Conversion Start Control (not active)
+// - CONV_RATE:    ADC Conversion Rate Selection (one shot)
+// - BOOST_FREQ:   Boost Mode Frequency Selection (500kHz)
+// - ICO_EN:       Input Current Optimizer (ICO) Enable (enabled)
+// - HVDCP_EN:     High Voltage DCP Enable (enabled)
+// - MAXC_EN:      MaxCharge Adapter Enable (enabled)
+// - FORCE_DPDM:   Force D+/D- Detection (disabled)
+// - AUTO_DPDM_EN: Automatic D+/D- Detection Enable when VBUS plugged in (enabled)
+// Reg3 (R/W)
+// - BAT_LOADEN: Battery Load (IBATLOAD) Enable (disabled)
+// - WD_RST:     I2C Watchdog Timer Reset (disabled)
+// - OTG_CONFIG: Boost (OTG) Mode Configuration (enabled) [FIXME!]
+// - CHG_CONFIG: Charge Enable Configuration (enabled, Battery charging is enabled by setting CHG_CONFIG bit, /CE pin is low and ICHG register is not 0 mA)
+// - SYS_MIN:    Minimum System Voltage Limit 3V-3.7V (3.5V) (Vsys == VDDH on nrf, VDDH-VDD>=0.3V, so if VDD=3.3V, Vsys=3.6V)
+// Reg4 (R/W)
+// - EN_PUMPX: Current pulse control Enable (disabled)
+// - ICHG:     Fast Charge Current Limit 0-5.056A (2.048A)
+// Reg5 (R/W)
+// - IPRECHG: Precharge Current Limit 64mA - 1024mA (128mA)
+// - ITERM:   Termination Current Limit 64mA - 1024mA (256mA)
+// Reg6 (R/W)
+// - VREG:    Charge Voltage Limit 3.840V - 4.608V (4.208V)
+// - BATLOWV: Battery Precharge to Fast Charge Threshold 2.8V-3.0V (3.0V)
+// - VRECHG:  Battery Recharge Threshold Offset 0.1V-0.2V (0.1V)
+// Reg7 (R/W)
+// - EN_TERM:   Charging Termination Enable (enabled)
+// - STAT_DIS:  STAT Pin Disable (pin enabled)
+// - WATCHDOG:  I2C Watchdog Timer Setting disabled-160s (40s)
+// - EN_TIMER:  Charging Safety Timer Enable (enabled)
+// - CHG_TIMER: Fast Charge Timer Setting 5hrs-20hrs (12hrs)
+// Reg8 (R/W)
+// - BAT_COMP: IR Compensation Resistor Setting 0-140mOhm (0)
+// - VCLAMP:   IR Compensation Voltage Clamp 0-0.224V (0)
+// - TREG:     Thermal Regulation Threshold 60-120C (120C)
+// Reg9 (R/W)
+// - FORCE_ICO:     Force Start Input Current Optimizer (ICO) (off)
+// - TMR2X_EN:      Safety Timer Setting during DPM or Thermal Regulation (slowed down by 2X)
+// - BATFET_DIS:    Force BATFET off to enable ship mode (disabled) [enables ship mode!]
+// - BATFET_DLY:    BATFET turn off delay control, tSM_DLY=10s-15s (off, no delay) 
+// - BATFET_RST_EN: BATFET full system reset enable (enabled)
+// - PUMPX_UP:      Current pulse control voltage up enable (disabled)
+// - PUMPX_DN:      Current pulse control voltage down enable	(disabled)
+// RegA (R/W)
+// - BOOSTV: Boost Mode Voltage Regulation 4.55V - 5.51V (5.126V)
+// RegB (R)
+// - VBUS_STAT: VBUS Status register
+// - CHRG_STAT: Charging Status
+// - PG_STAT:   Power Good Status
+// - SDP_STAT:  USB Input Status
+// - VSYS_STAT: VSYS Regulation Status
+// RegC (R)
+// - WATCHDOG_FAULT: Watchdog Fault Status
+// - BOOST_FAULT:    Boost Mode Fault Status
+// - CHRG_FAULT:     Charge Fault Status
+// - BAT_FAULT:      Battery Fault Status
+// - NTC_FAULT:      NTC Fault Status
+// RegD (R/W)
+// - FORCE_VINDPM: VINDPM Threshold Setting Method (relative)
+// - VINDPM:       Absolute VINDPM Threshold 3.9V - 15.3V (4.4V)
+// RegE (R)
+// - THERM_STAT: Thermal Regulation Status
+// - BATV:       ADC conversion of Battery Voltage (VBAT)
+// RegF (R)
+// - SYSV: ADC conversion of System Voltage (VSYS)
+// Reg10 (R)
+// - TSPCT: ADC conversion of TS Voltage (TS) as percentage of REGN
+// Reg11 (R)
+// - VBUS_GD: VBUS Good Status
+// - VBUSV:   ADC conversion of VBUS voltage (VBUS) [good if 12V!]
+// Reg12 (R)
+// - ICHGR: ADC conversion of Charge Current (IBAT) when VBAT > VBATSHORT
+// Reg13 (R)
+// - VDPM_STAT: VINDPM Status
+// - IDPM_STAT: IINDPM Status
+// - IDPM_LIM:  Input Current Limit in effect while Input Current Optimizer
+// Reg14(R/W R)
+// - REG_RST: Register Reset (0)
+// - ICO_OPTIMIZED: Input Current Optimizer (ICO) Status
+// - PN:            Device Configuration
+// - TS_PROFILE:    Temperature Profile
+// - DEV_REV:       Device Revision
--- modules/charger/charger_bq25895.c
+++ modules/charger/charger_bq25895.c
@@ -1,273 +1,235 @@
-#define DT_DRV_COMPAT ti_bq25895
+#define DT_DRV_COMPAT zmk_bq25895
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/charger.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/drivers/sensor.h>
 
-LOG_MODULE_REGISTER(bq25895, CONFIG_CHARGER_LOG_LEVEL);
+LOG_MODULE_REGISTER(bq25895, CONFIG_BQ25895_LOG_LEVEL);
 
+#define BOOLF(name, register, bit, extra)
+#define BOOL(name, register, bit, extra) int name;
+#define INT(name, register, startbit, endbit, offset, scale, extra) int name;
+#define INTA(name, register, startbit, endbit, offset, scale, extra) int name;
+#include "bq25895_regs.h"
 struct bq25895_config {
 	struct i2c_dt_spec i2c;
-	// uint32_t initial_current_microamp;
-	// uint32_t max_voltage_microvolt;
-	// uint32_t recharge_voltage_microvolt;
-	// uint32_t precharge_threshold_voltage_microvolt;
+    struct gpio_dt_spec irq_gpio;
+	// all values shifted, as set in I2C registers
+	BQ_CFG(0)
+};
 
+struct bq25895_data {
+    BQ_ADC(0)
+};
 
+#undef BOOL
+#undef INT
+#undef BOOLF
+#undef INTA
+
+#ifdef CONFIG_BQ25895_LOG_LEVEL_DBG
+#define INT(name, register, startbit, endbit, offset, scale, extra) \
+dev_val = ((regs[register] & GENMASK(endbit, startbit))>> startbit) * scale + offset; \
+cfg_val = (cfg->name >> startbit) * scale + offset; \
+LOG_DBG("%s is set to %d, expect %d", #name, dev_val, cfg_val); 
+#define BOOL(name, register, bit, extra) INT(name, register, bit, bit, 0, 1, extra)
+#define INTA(name, register, startbit, endbit, offset, scale, extra) \
+dev_val = ((regs[register-0xe] & GENMASK(endbit, startbit))>> startbit) * scale + offset; \
+LOG_DBG("%s is measured at %d", #name, dev_val);
+#include "bq25895_regs.h"
+static void dump_settings(const struct device *dev, uint8_t* regs) {
+	const struct bq25895_config *cfg = dev->config;
+	int cfg_val, dev_val;
+	BQ_CFG(0)
 }
 
-#define BOOL(name, register, bit) int name;
-#define INT (name, register, startbit, endbit, offset, multiplier, unit) int name
-struct bq25895_status {
-	#include "bq25895_regs.h"
+static void dump_adc(const struct device *dev, uint8_t* regs) {
+	int dev_val;
+	BQ_ADC(0)
 }
 #undef BOOL
 #undef INT
+#undef BOOLF
+#undef INTA
+#endif //CONFIG_BQ25895_LOG_LEVEL_DBG
 
-struct int bq25895_get_status(const struct device *dev, bq25895_status *status) {
+#define BOOLF(name, register, bit, extra) \
+static int set_##name(const struct bq25895_config *cfg) { \
+	return i2c_reg_update_byte_dt(&cfg->i2c, register, GENMASK(bit,bit), GENMASK(bit,bit)); \
+}
+#define INT(name, register, startbit, endbit, offset, scale, extra) \
+	if (cfg->name != (GENMASK(endbit, startbit) & regs[register])) { \
+		LOG_DBG("Updating %s to %d", #name, cfg->name); \
+		ret = i2c_reg_update_byte_dt(&cfg->i2c, register, GENMASK(endbit, startbit), cfg->name); \
+		if (ret < 0) { return ret; } \
+	}
+#define BOOL(name, register, bit, extra) INT(name, register, bit, bit, 0, 1, extra)
+#define INTA(name, register, startbit, endbit, offset, scale, extra) \
+	drv_data->name = ((GENMASK(endbit, startbit) & regs[register - 0xE]) >> startbit) * scale + offset;
+
+#include "bq25895_regs.h"
+
+BQ_ACTION(0)
+
+static int bq25895_init(const struct device *dev) {
 	const struct bq25895_config *cfg = dev->config;
+	uint8_t val;
+	int ret;
+
+	// check readiness of irq gpio pin
+    if (!device_is_ready(cfg->irq_gpio.port)) {
+        LOG_ERR("IRQ GPIO device not ready!");
+        return -ENODEV;
+    }
+
+	if (!device_is_ready(cfg->i2c.bus)) {
+        LOG_WRN("I2C BUS not ready!");
+        return -ENODEV;
+    }
+
+	// init the irq pin
+    ret = gpio_pin_configure_dt(&cfg->irq_gpio, GPIO_INPUT);
+    if (ret) {
+        LOG_ERR("Cannot configure IRQ GPIO");
+        return ret;
+    }
+
+    // setup and add the irq callback associated
+    // gpio_init_callback(&data->irq_gpio_cb, pmw3610_gpio_callback, BIT(cfg->irq_gpio.pin));
+
+    // err = gpio_add_callback(config->irq_gpio.port, &data->irq_gpio_cb);
+    // if (err) {
+    //     LOG_ERR("Cannot add IRQ GPIO callback");
+    // }
+
+	uint8_t regs[0x15];
+	ret = i2c_burst_read_dt(&cfg->i2c, 0, (uint8_t *)&regs, sizeof(regs)/sizeof(uint8_t));
+	if (ret < 0) {
+		LOG_ERR("Error reading registers: %02x", ret);
+		return ret;
+	}
+
+	#ifdef CONFIG_BQ25895_LOG_LEVEL_DBG
+	for (int i=0; i<sizeof(regs)/sizeof(uint8_t); i++) {
+		//LOG_DBG("Reg BQ25895 %d set to %d", i, regs[i]);
+	}
+	dump_settings(dev, regs);
+	#endif //CONFIG_BQ25895_LOG_LEVEL_DBG
+
+	val = (regs[0x14]&GENMASK(5,3))>>3;
+	if (val != 0b000111) {
+		LOG_ERR("Invalid device id: %02x", val);
+		return -EINVAL;
+	}
+
+	// Put bq25895 into host mode
+	ret = set_wd_rst(cfg);
+	if (ret < 0) {
+		return ret;
+	}
+
+	// Correct any mismatched config values
+	BQ_CFG(0);
+
+	#ifdef CONFIG_BQ25895_LOG_LEVEL_DBG
+	ret = i2c_burst_read_dt(&cfg->i2c, 0, (uint8_t *)&regs, sizeof(regs)/sizeof(uint8_t));
+	if (ret < 0) {
+		return ret;
+	}
+	dump_settings(dev, regs);
+	#endif //CONFIG_BQ25895_LOG_LEVEL_DBG
+	return 0;
+}
+
+static int bq25895_sample_fetch(const struct device *dev, enum sensor_channel chan) {
+	bool valid_chan = chan == SENSOR_CHAN_GAUGE_STATE_OF_CHARGE || chan == SENSOR_CHAN_GAUGE_VOLTAGE || chan == SENSOR_CHAN_ALL;
+	if (!valid_chan) {
+        LOG_DBG("unsupported channel %d", chan);
+        return -ENOTSUP;
+    }
+
+	const struct bq25895_config *cfg = dev->config;
+    int ret;
+	ret = set_conv_start(cfg);
+	k_sleep(K_SECONDS(1));
 	
-	char regs[0x15];
-	for (int r = 0; r<0x15; r++) {
-		int ret = i2c_reg_read_byte_dt(&cfg->i2c, r, &regs[r]);
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	#define BOOL(name, register, bit) \
-	 status->name = regs[register] == BIT(bit);
-	#define INT (name, register, startbit, endbit, offset, multiplier, unit) \
-	 status->name = offset + multiplier * ((regs[register] & GENMASK(startbit, endbit))<<startbit);
-	#include "bq25895_regs.h"
-	#undef BOOL
-	#undef INT
-
-	return 0;
-}
-
-/*
- * For ICHG <= 35mA = ICHGCODE + 5mA
- * For ICHG > 35mA = 40 + ((ICHGCODE-31)*10)mA.
- * Maximum programmable current = 1000mA
- *
- * Return: value between 0 and 127, negative on error.
- */
-static int bq25895_ma_to_ichg(uint32_t current_ma, uint8_t *ichg)
-{
-	if (!IN_RANGE(current_ma, BQ25895_CURRENT_MIN_MA, BQ25895_CURRENT_MAX_MA)) {
-		LOG_WRN("charging current out of range: %dmA, "
-			"clamping to the nearest limit", current_ma);
-	}
-	current_ma = CLAMP(current_ma, BQ25895_CURRENT_MIN_MA, BQ25895_CURRENT_MAX_MA);
-
-	if (current_ma <= 35) {
-		*ichg = current_ma - 5;
-		return 0;
-	}
-
-	*ichg = (current_ma - 40) / 10 + 31;
-
-	return 0;
-}
-
-static uint32_t bq25895_ichg_to_ma(uint8_t ichg)
-{
-	ichg &= BQ25895_ICHG_MSK;
-
-	if (ichg <= 30) {
-		return (ichg + 5);
-	}
-
-	return (ichg - 31) * 10 + 40;
-}
-
-static int bq25895_mv_to_vbatreg(const struct bq25895_config *cfg, uint32_t voltage_mv,
-				 uint8_t *vbat)
-{
-	if (!IN_RANGE(voltage_mv, BQ25895_VOLTAGE_MIN_MV, cfg->max_voltage_microvolt)) {
-		LOG_WRN("charging voltage out of range: %dmV, "
-			"clamping to the nearest limit",
-			voltage_mv);
-	}
-	voltage_mv = CLAMP(voltage_mv, BQ25895_VOLTAGE_MIN_MV, cfg->max_voltage_microvolt);
-
-	*vbat = (voltage_mv - BQ25895_VOLTAGE_MIN_MV) / BQ25895_FACTOR_VBAT_TO_MV;
-
-	return 0;
-}
-
-static uint32_t bq25895_vbatreg_to_mv(uint8_t vbat)
-{
-	vbat &= BQ25895_VBAT_MSK;
-
-	return (vbat * BQ25895_FACTOR_VBAT_TO_MV) + BQ25895_VOLTAGE_MIN_MV;
-}
-
-static int bq25183_charge_enable(const struct device *dev, const bool enable)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t value = enable ? 0 : BQ25895_ICHG_CHG_DIS;
-	int ret;
-
-	ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_ICHG_CTRL,
-				     BQ25895_ICHG_CHG_DIS, value);
+	uint8_t regs[0x15]; ret = i2c_burst_read_dt(&cfg->i2c, 0, (uint8_t *)&regs, sizeof(regs)/sizeof(uint8_t));
+	//uint8_t regs[5];
+	//ret = i2c_burst_read_dt(&cfg->i2c, 0x0E, (uint8_t *)&regs, sizeof(regs)/sizeof(uint8_t));
 	if (ret < 0) {
+		LOG_ERR("Error reading registers: %02x", ret);
 		return ret;
 	}
 
+	#ifdef CONFIG_BQ25895_LOG_LEVEL_DBG
+	for (int i=0; i<sizeof(regs)/sizeof(uint8_t); i++) {
+		//LOG_DBG("ADC Reg %d set to %d", i+0xE, regs[i]);
+		LOG_DBG("Reg %d set to %d", i, regs[i]);
+	}
+	dump_settings(dev, regs);
+	dump_adc(dev, regs);
+	#endif //CONFIG_BQ25895_LOG_LEVEL_DBG
+
+	// set drv_data
+	struct bq25895_data *const drv_data = dev->data;
+	BQ_ADC(0);
 	return 0;
 }
+#undef BOOL
+#undef INT
+#undef BOOLF
+#undef INTA
 
-static int bq25895_set_charge_current(const struct device *dev,
-				      uint32_t const_charge_current_ua)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
+static int bq25895_channel_get(const struct device *dev, enum sensor_channel chan,
+                                struct sensor_value *val) {
+    int err = 0;
+	
+	struct bq25895_data *const drv_data = dev->data;
+    int bat_voltage = drv_data->batv;
+	LOG_DBG("ADC bat_voltage is measured at %d mV", bat_voltage);
 
-	ret = bq25895_ma_to_ichg(const_charge_current_ua / 1000, &val);
-	if (ret < 0) {
-		return ret;
-	}
+    switch (chan) {
+    case SENSOR_CHAN_GAUGE_VOLTAGE:
+		LOG_DBG("ADC+ bat_voltage is measured at %d mV", bat_voltage);
+        // 1250 / 16 = 78.125
+        uint64_t tmp = bat_voltage * 1250 / 16;
+        val->val1 = tmp / 1000000;
+        val->val2 = tmp % 1000000;
+        break;
 
-	ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_ICHG_CTRL,
-				     BQ25895_ICHG_MSK, val);
-	if (ret < 0) {
-		return ret;
-	}
+    case SENSOR_CHAN_GAUGE_STATE_OF_CHARGE:
+		LOG_DBG("ADC- bat_voltage is measured at %d mV", bat_voltage);
+        val->val1 = (bat_voltage >> 8);
+        val->val2 = (bat_voltage & 0xFF) * 1000000 / 256;
+        break;
 
-	return 0;
+    default:
+        err = -ENOTSUP;
+        break;
+    }
+
+    return err;
 }
 
-static int bq25895_get_charge_current(const struct device *dev,
-				      uint32_t *const_charge_current_ua)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
+static const struct sensor_driver_api bq25895_sensor_api = {
+	.sample_fetch = bq25895_sample_fetch,
+	.channel_get = bq25895_channel_get
+};
 
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_ICHG_CTRL, &val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	*const_charge_current_ua = bq25895_ichg_to_ma(val) * 1000;
-
-	return 0;
-}
-
-static int bq25895_set_charge_voltage(const struct device *dev, uint32_t const_charge_voltage_uv)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
-
-	ret = bq25895_mv_to_vbatreg(cfg, const_charge_voltage_uv / 1000, &val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_VBAT_CTRL, BQ25895_VBAT_MSK, val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	return 0;
-}
-
-static int bq25895_get_charge_voltage(const struct device *dev, uint32_t *const_charge_voltage_uv)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
-
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_VBAT_CTRL, &val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	*const_charge_voltage_uv = bq25895_vbatreg_to_mv(val) * 1000;
-
-	return 0;
-}
-
-static int bq25895_get_online(const struct device *dev,
-			      enum charger_online *online)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
-
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_STAT0, &val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if ((val & BQ25895_STAT0_VIN_PGOOD_STAT) != 0x00) {
-		*online = CHARGER_ONLINE_FIXED;
-	} else {
-		*online = CHARGER_ONLINE_OFFLINE;
-	}
-
-	return 0;
-}
-
-static int bq25895_get_status(const struct device *dev,
-			      enum charger_status *status)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t stat0;
-	uint8_t ichg_ctrl;
-	int ret;
-
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_STAT0, &stat0);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if ((stat0 & BQ25895_STAT0_VIN_PGOOD_STAT) == 0x00) {
-		*status = CHARGER_STATUS_DISCHARGING;
-		return 0;
-	}
-
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_ICHG_CTRL, &ichg_ctrl);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if ((ichg_ctrl & BQ25895_ICHG_CHG_DIS) != 0x00) {
-		*status = CHARGER_STATUS_NOT_CHARGING;
-		return 0;
-	}
-
-	switch (FIELD_GET(BQ25895_STAT0_CHG_STAT_MASK, stat0)) {
-	case BQ25895_STAT0_CHG_STAT_NOT_CHARGING:
-		*status = CHARGER_STATUS_NOT_CHARGING;
-		break;
-	case BQ25895_STAT0_CHG_STAT_CONSTANT_CURRENT:
-	case BQ25895_STAT0_CHG_STAT_CONSTANT_VOLTAGE:
-		*status = CHARGER_STATUS_CHARGING;
-		break;
-	case BQ25895_STAT0_CHG_STAT_DONE:
-		*status = CHARGER_STATUS_FULL;
-		break;
-	}
-
-	return 0;
-}
-
+#if 0 // Charger API isnt used in ZMK; also no clear multi-API device support in zephyr 
 static int bq25895_get_prop(const struct device *dev, charger_prop_t prop,
 			    union charger_propval *val)
 {
 	switch (prop) {
-	case CHARGER_PROP_ONLINE:
-		return bq25895_get_online(dev, &val->online);
-	case CHARGER_PROP_STATUS:
-		return bq25895_get_status(dev, &val->status);
+	// case CHARGER_PROP_ONLINE:
+	// 	return bq25895_get_online(dev, &val->online);
+	// case CHARGER_PROP_STATUS:
+	// 	return bq25895_get_status(dev, &val->status);
 	// case CHARGER_PROP_CONSTANT_CHARGE_CURRENT_UA:
 	// 	return bq25895_get_charge_current(dev, &val->const_charge_current_ua);
 	// case CHARGER_PROP_CONSTANT_CHARGE_VOLTAGE_UV:
@@ -290,93 +252,42 @@ static int bq25895_set_prop(const struct device *dev, charger_prop_t prop,
 	}
 }
 
-static const struct charger_driver_api bq25895_api = {
+static const struct charger_driver_api bq25895_charger_api = {
 	.get_property = bq25895_get_prop,
 	.set_property = bq25895_set_prop,
 	// .charge_enable = bq25183_charge_enable,
 };
+#endif
 
-static int bq25895_init(const struct device *dev)
-{
-	const struct bq25895_config *cfg = dev->config;
-	uint8_t val;
-	int ret;
+#define INT(name, register, startbit, endbit, offset, scale, inst) \
+. name = GENMASK(endbit, startbit) & ((DT_INST_PROP(inst, name)-offset)/scale)<<startbit,
+#define BOOL(name, register, bit, inst) INT(name, register, bit, bit, 0, 1, inst)
+#define INTA(name, register, startbit, endbit, offset, scale, inst) \
+. name = 0,
+#include "bq25895_regs.h"
+// #undef BOOL
+// #undef INT
 
-	ret = i2c_reg_read_byte_dt(&cfg->i2c, BQ25895_MASK_ID, &val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	val &= BQ25895_DEVICE_ID_MSK;
-	if (val != BQ25895_DEVICE_ID) {
-		LOG_ERR("Invalid device id: %02x", val);
-		return -EINVAL;
-	}
-
-	/* Disable the watchdog */
-	ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_IC_CTRL,
-				     BQ25895_WATCHDOG_SEL_1_MSK,
-				     BQ25895_WATCHDOG_DISABLE);
-	if (ret < 0) {
-		return ret;
-	}
-
-	ret = bq25895_set_charge_voltage(dev, cfg->max_voltage_microvolt);
-	if (ret < 0) {
-		LOG_ERR("Could not set the target voltage. (rc: %d)", ret);
-		return ret;
-	}
-
-	if (cfg->recharge_voltage_microvolt > 0) {
-		if ((cfg->max_voltage_microvolt - cfg->recharge_voltage_microvolt) > 100000) {
-			val = BQ25895_IC_CTRL_VRCH_200;
-		} else {
-			val = BQ25895_IC_CTRL_VRCH_100;
-		}
-
-		ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_IC_CTRL, BQ25895_IC_CTRL_VRCH_MSK,
-					     val);
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	/* Precharge threshold voltage */
-	if (cfg->precharge_threshold_voltage_microvolt <= 2800000) {
-		val = BQ25895_VLOWV_SEL_2_8;
-	} else {
-		val = BQ25895_VLOWV_SEL_3_0;
-	}
-
-	ret = i2c_reg_update_byte_dt(&cfg->i2c, BQ25895_IC_CTRL, BQ25895_VLOWV_SEL_MSK, val);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if (cfg->initial_current_microamp > 0) {
-		ret = bq25895_set_charge_current(dev, cfg->initial_current_microamp);
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
-#define CHARGER_BQ25895_INIT(inst)                                                                 \
-	static const struct bq25895_config bq25895_config_##inst = {                               \
-		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
-		.initial_current_microamp =                                                        \
-			DT_INST_PROP(inst, constant_charge_current_max_microamp),                  \
-		.max_voltage_microvolt =                                                           \
-			DT_INST_PROP(inst, constant_charge_voltage_max_microvolt),                 \
-		.recharge_voltage_microvolt =                                                      \
-			DT_INST_PROP_OR(inst, re_charge_voltage_microvolt, 0),                     \
-		.precharge_threshold_voltage_microvolt =                                           \
-			DT_INST_PROP(inst, precharge_voltage_threshold_microvolt),                 \
-	};                                                                                         \
-                                                                                                   \
-	DEVICE_DT_INST_DEFINE(inst, bq25895_init, NULL, NULL, &bq25895_config_##inst, POST_KERNEL, \
-			      CONFIG_CHARGER_INIT_PRIORITY, &bq25895_api);
+#define CHARGER_BQ25895_INIT(inst)                                      \
+	static const struct bq25895_config bq25895_config_##inst = {        \
+		.i2c = I2C_DT_SPEC_INST_GET(inst),                              \
+		.irq_gpio = GPIO_DT_SPEC_INST_GET(inst, irq_gpios),             \
+		BQ_CFG(inst)                                                    \
+	};                                                                  \
+	static struct bq25895_data bq25895_data_##inst = {                  \
+		BQ_ADC(inst)                                                    \
+	};                                                                  \
+                                                                        \
+	DEVICE_DT_INST_DEFINE(inst, bq25895_init, NULL, &bq25895_data_##inst,\
+	    &bq25895_config_##inst, POST_KERNEL, CONFIG_CHARGER_INIT_PRIORITY, &bq25895_sensor_api);
 
 DT_INST_FOREACH_STATUS_OKAY(CHARGER_BQ25895_INIT)
+
+
+// static const struct sensor_driver_api bq25895_api_table = {.sample_fetch = bq25895_sample_fetch,
+//                                                             .channel_get = bq25895_channel_get};
+//     DEVICE_DT_INST_DEFINE(inst, bq25895_init, NULL, &bq25895_##inst##_drvdata,                   
+//                           &bq25895_##inst##_config, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,     
+//                           &bq25895_api_table);
+
+// DT_INST_FOREACH_STATUS_OKAY(MAX17048_INIT)
